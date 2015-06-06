f5a0c228fbddb67b879d08731228b0f2a142da9f
Eugene Sharygin <eush77@gmail.com>
2015-06-06 04:28:56 +0300

    Untangle the main part

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
 #! /usr/bin/env node
+// -*- js2-strict-missing-semi-warning: nil; -*-
 
-var resolve = require('resolve')
-var pager   = require('default-pager')
-var fs      = require('fs')
-var path    = require('path')
-var opts    = require('optimist').argv
-var toUrl   = require('github-url').toUrl
-var opener  = require('opener')
-var rc      = require('rc')
-var apidocs = require('node-api-docs')
+var resolve  = require('resolve')
+var pager    = require('default-pager')
+var fs       = require('fs')
+var path     = require('path')
+var optimist = require('optimist')
+var toUrl    = require('github-url').toUrl
+var opener   = require('opener')
+var rc       = require('rc')
+var apidocs  = require('node-api-docs')
 
-var name = opts._.shift()
-var global = opts.g || opts.global
 
-if(!name) {
-    name = './'
+var basedir = function (opts) {
+  if (!opts.global) {
+    return process.cwd()
   }
+  var npmrc = rc('npm', null, [])
+  var prefix = npmrc.prefix || path.resolve(process.execPath, '../..')
+  return path.join(prefix, 'lib')
+}
 
-if(resolve.isCore(name)) {
-    return apidocs(name).pipe(pager())
+
+var packageFile = function (name, opts) {
+  return resolve.sync(path.join(name, 'package.json'), { basedir: basedir(opts) })
+}
+
+
+var packageReadme = function (name, opts) {
+  var pkg = packageFile(name, opts)
+  var pkgDir = path.dirname(pkg)
+  var readmes = fs.readdirSync(pkgDir).filter(function (filename) {
+    return /^readme/.test(filename.toLowerCase())
+  })
+  if (!readmes.length) {
+    throw new Error('Readme not found in ' + pkgDir)
   }
+  return fs.createReadStream(path.join(pkgDir, readmes[0]))
+}
+
+
+var packageUrl = function (pkg, webUrl) {
+  var repoUrl = toUrl(pkg.repository)
+  return webUrl
+    ? pkg.homepage || repoUrl || 'https://npm.im/' + pkg.name
+    : repoUrl
+}
+
 
 try {
-  var found = false
-  var prefix = rc('npm').prefix || path.join(process.execPath, '../..')
-  var file = resolve.sync(name, {
-    basedir: global ? path.join(prefix, 'lib') : process.cwd(),
-    packageFilter: function (pkg, dir) {
-      if(opts.git || opts.github || opts.gh) {
-        opener(toUrl(pkg.repository))
-        return found = true
-      }
-      if(opts.web) {
-        opener(pkg.homepage || toUrl(pkg.repository) || 'https://npm.im/'+pkg.name)
-        return found = true
+  var opts = optimist.argv
+  var name = String(opts._.shift()) || './'
 
-      }
-      var l = fs.readdirSync(dir)
-      while(l.length) {
-        var f = l.shift()
-        if(/^readme/.test(f.toLowerCase())) {
-          found = true
-          fs.createReadStream(path.join(dir, f)).pipe(pager())
-        }
-      }
-      return pkg
-    }})
-} catch(e) {
-  if(found) return
+  if (opts.g) {
+    opts.global = true
+  }
+
+  if (resolve.isCore(name)) {
+    apidocs(name).pipe(pager())
+  } else if (opts.github || opts.gh || opts.web) {
+    var pkg = require(packageFile(name, opts))
+    opener(packageUrl(pkg, opts.web))
+  } else {
+    packageReadme(name, opts).pipe(pager())
+  }
+} catch (e) {
   console.error(e.message)
   process.exit(1)
 }
-
